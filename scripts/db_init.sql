84d7243c36daaf5a5f1a1d639899726b8a849b8f
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2025-06-21 17:23:57 +0530

    DB Initialization done

---
+++ scripts/db_init.sql
@@ -0,0 +1,15 @@
+-- Creating DataBase 'Data_Warehouse'. If it exists then drop the database and create a new one.
+DROP DATABASE IF EXISTS Data_Warehouse;
+CREATE DATABASE Data_Warehouse;
+
+USE Data_Warehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
